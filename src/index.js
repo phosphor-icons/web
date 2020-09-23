1fc7bf6a07c65d01591a6a29bc6e482d4f323f62
rektdeckard <friedtm@gmail.com>
2020-09-23 16:18:29 -0400

    lib: direct CSS font source to v0.2.17

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "./css/ph.css";
+var head = document.getElementsByTagName("head")[0];
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "https://unpkg.com/phosphor-icons@0.2.17/src/css/ph.css";
+head.appendChild(link);
