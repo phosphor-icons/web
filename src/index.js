174cdab0978c390244a261881ee3e017a637998d
rektdeckard <friedtm@gmail.com>
2020-11-24 19:36:13 -0500

    icons+meta: fix CloudMoon missing weights and update to v1.1.1

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ var head = document.getElementsByTagName("head")[0];
 var link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
-link.href = "https://unpkg.com/phosphor-icons@1.1.0/src/css/phosphor.css";
+link.href = "https://unpkg.com/phosphor-icons@1.1.1/src/css/phosphor.css";
 head.appendChild(link);
