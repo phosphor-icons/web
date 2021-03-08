48f1de229987fc7c8160e21dbc87f9963b4a5ef8
rektdeckard <friedtm@gmail.com>
2021-03-08 00:52:43 -0500

    icons+meta: Update assets and icons to v1.2.0

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ var head = document.getElementsByTagName("head")[0];
 var link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
-link.href = "https://unpkg.com/phosphor-icons@1.1.3/src/css/phosphor.css";
+link.href = "https://unpkg.com/phosphor-icons@1.2.0/src/css/phosphor.css";
 head.appendChild(link);
