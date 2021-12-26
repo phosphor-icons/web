b4ef91637b6b36040562170a9e58247a12a94469
rektdeckard <friedtm@gmail.com>
2021-12-26 13:17:40 -0500

    hotfix(pkg): correct version

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ var head = document.getElementsByTagName("head")[0];
 var link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
-link.href = "https://unpkg.com/phosphor-icons@1.4.0/src/css/icons.css";
+link.href = "https://unpkg.com/phosphor-icons@1.4.2/src/css/icons.css";
 head.appendChild(link);
