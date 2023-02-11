e534f06a0b9703cca296e727be3649d7ad805669
rektdeckard <friedtm@gmail.com>
2023-02-11 14:48:30 -0700

    chore(core): add submodule

--- bin/collate.js
+++ bin/collate.js
@@ -2,90 +2,95 @@
 const fs = require("fs");
 const path = require("path");
 const chalk = require("chalk");
+const { exec } = require("child_process");
 
-let { ASSETS_PATH } = require("./index");
+const { ASSETS_PATH } = require("./index");
 
-if (process.argv[2]) {
-  const dest = process.argv[2];
-  if (fs.existsSync(dest) && fs.lstatSync(dest).isDirectory()) {
-    ASSETS_PATH = dest;
-  }
-}
+const icons = {};
+const weights = ["thin", "light", "regular", "bold", "fill", "duotone"];
 
-function moveToAndCreateFolderIfNeeded(fileName, folderName) {
-  const oldPath = path.join(ASSETS_PATH, fileName);
-  const folderPath = path.join(ASSETS_PATH, folderName);
-  const newPath = path.join(folderPath, fileName);
+main();
 
-  // Make directory if needed
-  if (!fs.existsSync(folderPath) || !fs.lstatSync(folderPath).isDirectory()) {
-    fs.mkdirSync(folderPath);
-  }
-
-  move(oldPath, newPath);
-}
-
-function move(oldPath, newPath) {
-  fs.rename(oldPath, newPath, (err) => {
-    if (err) {
-      if (err.code === "EXDEV") {
-        console.error(
-          `${chalk.inverse.red(" FAIL ")} ${newPath} already exists`
-        );
-        copy();
-      } else {
-        console.error(
-          `${chalk.inverse.red(" FAIL ")} ${newPath} could not be created`
-        );
+function main() {
+  exec(
+    "git submodule update --remote --init --recursive",
+    (err, _stdout, stderr) => {
+      if (err) {
+        console.error(`${chalk.inverse.red(" ERR ")} ${err.message}`);
+        process.exit(1);
       }
+
+      if (stderr) {
+        console.error(`${chalk.inverse.red(" ERR ")} ${stderr}`);
+        process.exit(1);
+      }
+
+      console.log(
+        `${chalk.inverse.green(" OK ")} Updated submodule @phosphor-icons/core`
+      );
+
+      loadWeights();
+    }
+  );
+}
+
+function readFile(pathname, name, weight) {
+  const file = fs.readFileSync(pathname);
+  icons[name][weight] = file
+    .toString("utf-8")
+    .replace(/^.*<\?xml.*?\>/g, "")
+    .replace(/<svg.*?>/g, "")
+    .replace(/<\/svg>/g, "")
+    .replace(
+      /<rect width="25[\d,\.]+" height="25[\d,\.]+" fill="none".*?\/>/g,
+      ""
+    )
+    .replace(/<title.*?/, "")
+    .replace(/"#0+"/g, "{color}")
+    .replace(/fill\-rule/g, "fillRule")
+    .replace(/stroke-linecap/g, "strokeLinecap")
+    .replace(/stroke-linejoin/g, "strokeLinejoin")
+    .replace(/stroke-width/g, "strokeWidth")
+    .replace(/stroke-miterlimit/g, "strokeMiterlimit");
+}
+
+function loadWeights() {
+  const weightFolder = fs.readdirSync(ASSETS_PATH, "utf-8");
+
+  weightFolder.forEach((weightFolder) => {
+    if (!fs.lstatSync(path.join(ASSETS_PATH, weightFolder)).isDirectory())
       return;
+
+    if (!weights.includes(weightFolder)) {
+      console.error(
+        `${chalk.inverse.red(" ERR ")} Bad folder name ${weightFolder}`
+      );
+      process.exit(1);
     }
-  });
 
-  console.log(`${chalk.inverse.green(" DONE ")} ${newPath}`);
+    const files = fs.readdirSync(path.join(ASSETS_PATH, weightFolder));
+    files.forEach((filename) => {
+      let name;
+      const nameParts = filename.split(".svg")[0].split("-");
+      if (nameParts[nameParts.length - 1] === weightFolder) {
+        name = nameParts.slice(0, -1).join("-");
+      } else {
+        name = nameParts.join("-");
+      }
 
-  function copy() {
-    var readStream = fs.createReadStream(oldPath);
-    var writeStream = fs.createWriteStream(newPath);
-
-    readStream.on("error", callback);
-    writeStream.on("error", callback);
-
-    readStream.on("close", function () {
-      fs.unlink(oldPath, callback);
+      if (!icons[name]) {
+        icons[name] = {};
+      }
+      const filepath = path.join(ASSETS_PATH, weightFolder, filename);
+      readFile(filepath, name, weightFolder);
     });
-
-    readStream.pipe(writeStream);
-    console.log(`${chalk.inverse.green(" DONE ")} ${newPath}`);
-  }
-}
-
-function collate() {
-  const files = fs.readdirSync(ASSETS_PATH, "utf-8");
-
-  files.forEach((file) => {
-    // Only SVGs!
-    if (!file.match(/.*\.svg$/)) return;
-
-    let folder;
-
-    const filenameParts = file.split(".svg")[0].split("-");
-    const weightOrLastIdentifier = filenameParts.slice(-1)[0];
-    switch (weightOrLastIdentifier) {
-      case "thin":
-      case "light":
-      case "bold":
-      case "fill":
-      case "duotone":
-        folder = weightOrLastIdentifier;
-        break;
-      default:
-        folder = "regular";
-        break;
-    }
-
-    moveToAndCreateFolderIfNeeded(file, folder);
   });
 }
 
-collate();
+function checkFiles(icon) {
+  const weightsPresent = Object.keys(icon);
+  return (
+    weightsPresent.length === 6 &&
+    weightsPresent.every((w) => weights.includes(w))
+  );
+}
